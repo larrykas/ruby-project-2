6b9a3ebea941f42aca3ab1b64e0adc553be8087a
larrykas <larrykas@users.noreply.github.com>
2013-09-24 19:50:08 +0300

    Update hash.rb

--- hash.rb
+++ hash.rb
@@ -1,9 +1,14 @@
+# Creates a hash with the KEY being a name and the VALUE being the type of car a person drives
 property = {"Larry"=>"Aston Martin", "Jenny"=>"Bentley", "Larryn"=>"BMW", "Jared"=>"Porsche"}
-
+# outputs the KEYS and VALUES of the above hash
 puts property
-
+# changes the car Jenny drives
 property["Jenny"] = "Volvo"
-
+# Returns new look of the hash after the changes are made
 property.inspect
-
+# An enumerator is returned
 property.each
+# finds the value and returns the key
+property.index("BMW")
+
+
