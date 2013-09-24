d6a34a8211762af07e89d300186a8b5a857af139
larrykas <larrykas@users.noreply.github.com>
2013-09-24 19:55:42 +0300

    Update ArrayNames.rb

--- ArrayNames.rb
+++ ArrayNames.rb
@@ -1,11 +1,12 @@
+# Create an array of names
 names = ["Larry", "Jenny", "Larryn", "Lincoln"]
-
+# returns the array (ascending) in alphabetical order
 names.sort
-
+# Remove the first item from the array
 names.delete_at(0)
-
+# Remove the last item from the array
 names.pop
-
+# returns the array in reverse order
 names.reverse
-
-print names
+# print the names to the screen
+puts names
