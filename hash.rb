5c926e0bdcb2e12d543b24926fdb7c56a66bc9ca
larrykas <larrykas@users.noreply.github.com>
2013-09-24 12:51:43 +0300

    Create hash.rb

---
+++ hash.rb
@@ -0,0 +1,10 @@
+property = {"Larry"=>"Aston Martin", "Jenny"=>"Bentley", "Larryn"=>"BMW", "Jared"=>"Porsche"}
+
+print property
+
+property.delete("Jenny")
+
+property["Jenny"] = "Volvo"
+
+property.inspect
+
